3432125d9c7df9bc7a869172daed51249042c01e
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2023-12-13 23:26:25 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom"
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>Hello Vite</p>
+      </div>
+    </>
+  )
+}
+
+export default App
